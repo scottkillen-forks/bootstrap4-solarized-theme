730cc77c519324502a11773183cef6c1b2a477a3
Shigeru Hagiwara <hgwrsgr@gmail.com>
2019-08-12 15:11:28 +0900

    gulp executed successfully

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,57 @@
 // Assigning modules to local variables
-var gulp = require('gulp'),
-    scss = require('gulp-scss'),
-    scsslint = require('gulp-scss-lint'),
-    cssmin = require('gulp-cssmin'),
-    autoprefixer = require('gulp-autoprefixer'),
-    header = require('gulp-header'),
-    rename = require("gulp-rename"),
-    pkg = require('./package.json');
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  sasslint = require("gulp-sass-lint"),
+  cssmin = require("gulp-cssmin"),
+  autoprefixer = require("gulp-autoprefixer"),
+  header = require("gulp-header"),
+  rename = require("gulp-rename"),
+  pkg = require("./package.json");
 
 // Set the banner content
-var banner = ['/*!\n',
-    ' * Bootstrap Solarized - Official Website (<%= pkg.homepage %>)\n',
-    ' * Copyright 2016-' + (new Date()).getFullYear(), ' <%= pkg.author.name %>\n',
-    ' * Licensed under <%= pkg.license %>\n',
-    ' */\n',
-    ''
-].join('');
+var banner = [
+  "/*!\n",
+  " * Bootstrap4 Solarized - Official Website (<%= pkg.homepage %>)\n",
+  " * Copyright 2016-" + new Date().getFullYear(),
+  " <%= pkg.author.name %>\n",
+  " * Licensed under <%= pkg.license %>\n",
+  " */\n",
+  ""
+].join("");
 
-// scss task to compile the scss files and add the banner
-gulp.task('scss', function() {
-    return gulp
-      .src('./scss/*.scss')
-      .pipe(scss().on('error', scss.logError))
-      .pipe(header(banner, { pkg: pkg }))
-      .pipe(gulp.dest('dist/css'));
+// sass task to compile the sass files and add the banner
+gulp.task("sass", function() {
+  return gulp
+    .src('./sass/*.scss')
+    .pipe(sass().on("error", sass.logError))
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest("dist/css"));
 });
 
 // Minify CSS
-gulp.task('minify', gulp.series(gulp.parallel('scss')), function() {
-    return gulp
-      .src(['./dist/css/*.css','!./dist/css/*.min.css'])
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(autoprefixer())
-      .pipe(cssmin())
-      .pipe(gulp.dest('dist/css'));
+gulp.task("minify", gulp.series(gulp.parallel("sass")), function() {
+  return gulp
+    .src(["./dist/css/*.css", "!./dist/css/*.min.css"])
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(autoprefixer())
+    .pipe(cssmin())
+    .pipe(gulp.dest("dist/css"));
 });
 
-// Lint scss
-gulp.task('lint', function () {
-    return gulp
-      .src('scss/**/*.s+(a|c)ss')
-      .pipe(scsslint());
+// Lint SASS
+gulp.task("lint", function() {
+  return gulp
+    .src("sass/**/*.s+(a|c)ss")
+    .pipe(sasslint())
+    .pipe(sasslint.format())
+    .pipe(sasslint.failOnError());
 });
 
-// Watch task monitors scss files
-gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['scss']);
-    gulp.watch('dist/css/*.css', ['minify']);
+// Watch task monitors SASS files
+gulp.task("watch", function() {
+  gulp.watch("sass/*.scss", ["sass"]);
+  gulp.watch("dist/css/*.css", ["minify"]);
 });
 
 // Default task
-gulp.task('default', gulp.series(gulp.parallel('scss', 'minify')));
+gulp.task("default", gulp.series(gulp.parallel("sass", "minify")));
